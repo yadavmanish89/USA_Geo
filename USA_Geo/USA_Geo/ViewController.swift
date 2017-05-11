080b07b4cd6937abb9c9cd38ae3ac7d05fd4c9ca
yadavmanish89 <yadavmanish89@gmail.com>
2017-05-11 17:33:58 -0400

    Initial Commit

---
+++ USA_Geo/USA_Geo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  USA_Geo
+//
+//  Created by MANISH on 11/05/17.
+//  Copyright © 2017 Manish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
